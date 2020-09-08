054df3de25a8d513667de7acb4fa7e5afc4c2bb9
Yoshiki Nagasaki <nagatelu1020@gmail.com>
2020-09-08 20:23:39 +0900

    [day11] mouse pointer can move to the edge of window

--- src/bootpack.c
+++ src/bootpack.c
@@ -84,7 +84,6 @@ void HariMain(void)
           sheet_refresh(shtctl, sht_back, 32, 16, 32 + 15*8, 32);
 
 					/* move mouse cursor */
-					boxfill8(binfo->vram, binfo->scrnx, COL8_008484, mx, my, mx+15, my+15);
 					mx += mdec.x;
 					my += mdec.y;
 					if (mx < 0) {
@@ -93,11 +92,11 @@ void HariMain(void)
 					if (my < 0) {
 						my = 0;
 					}
-					if(mx > binfo->scrnx - 16) {
-						mx = binfo->scrnx - 16;
+					if(mx > binfo->scrnx - 1) {
+						mx = binfo->scrnx - 1;
 					}
-					if(my > binfo->scrny - 16) {
-						my = binfo->scrny - 16;
+					if(my > binfo->scrny - 1) {
+						my = binfo->scrny - 1;
 					}
 					sprintf(s, "(%3d, %3d)", mx, my);
 					boxfill8(buf_back, binfo->scrnx, COL8_008484, 0, 0, 79, 15);
